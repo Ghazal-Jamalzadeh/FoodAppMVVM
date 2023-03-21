e2d6c6fe26c7e2a02161ebe4a510ec0fa2d7a337
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-03-21 03:41:11 -0700

    added resources + bindings + navHost

--- app/src/main/java/com/jmzd/ghazal/foodappmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/foodappmvvm/ui/MainActivity.kt
@@ -2,11 +2,37 @@ package com.jmzd.ghazal.foodappmvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.jmzd.ghazal.foodappmvvm.R
+import com.jmzd.ghazal.foodappmvvm.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    //Binding
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    //nav controller
+    private lateinit var navHost: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHost = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navHost.navController.navigateUp() || super.onNavigateUp()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/jmzd/ghazal/foodappmvvm/ui/detail/player/PlayerActivity.kt
@@ -0,0 +1,13 @@
+
+package com.jmzd.ghazal.foodappmvvm.ui.detail.player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jmzd.ghazal.foodappmvvm.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}

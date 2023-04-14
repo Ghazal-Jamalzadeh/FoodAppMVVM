8824118f7554c0d29589e03cecc907a044ec3905
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-04-14 04:41:57 -0700

    added youtube player

--- app/src/main/java/com/jmzd/ghazal/foodappmvvm/ui/detail/player/PlayerActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/foodappmvvm/ui/detail/player/PlayerActivity.kt
@@ -1,13 +1,56 @@
 
 package com.jmzd.ghazal.foodappmvvm.ui.detail.player
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jmzd.ghazal.foodappmvvm.R
+import android.view.WindowManager
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.jmzd.ghazal.foodappmvvm.databinding.ActivityPlayerBinding
+import com.jmzd.ghazal.foodappmvvm.utils.VIDEO_ID
+import com.jmzd.ghazal.foodappmvvm.utils.YOUTUBE_API_KEY
 
-class PlayerActivity : AppCompatActivity() {
+
+@Suppress("DEPRECATION")
+class PlayerActivity : YouTubeBaseActivity() {
+    //Binding
+    private var _binding: ActivityPlayerBinding? = null
+    private val binding get() = _binding!!
+
+    //Other
+    private lateinit var player: YouTubePlayer
+    private var videoId = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        _binding = ActivityPlayerBinding.inflate(layoutInflater)
+        //Full screen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(binding.root)
+        //Get id
+        videoId = intent.getStringExtra(VIDEO_ID).toString()
+        //Player initialize
+        val listener = object : YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer, p2: Boolean) {
+                player = p1
+                player.loadVideo(videoId)
+                player.play()
+            }
+
+            override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+
+            }
+
+        }
+
+        binding.videoPlayer.initialize(YOUTUBE_API_KEY, listener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
     }
 }
